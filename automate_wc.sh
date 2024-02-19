63755ad723ad40fe7c678e53820e49212456c5f5
Jonathan Granda Acaro <jgrandaa@simulacrallm.virtual.crc.nd.edu>
2024-02-19 12:53:30 -0500

    Updated name on few files

--- automate_wc.sh
+++ automate_wc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Script is inted to be used to automate word count processing
 # Define an array to hold the file names
 files=()
 # Define the directory path
@@ -17,13 +18,8 @@ done
 
 # Loop through the array of files
 for file in "${files[@]}"; do
-    # Execute your program here, using "$file" as an argument if needed
-    # Replace 'your_program' with the actual program you want to run
-    # And 'some_argument' with any arguments your program needs, if any
     python3 analysis_convo.py "$file"
-    
-    # For example, if you're counting words in each file using 'wc':
-    # wc -w "$file"
+
 done
 
 
