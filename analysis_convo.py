8fd4bbd6db19c9eb928f2b702321394128cf0a5a
Jonathan Granda Acaro <jgrandaa@simulacrallm.virtual.crc.nd.edu>
2024-02-19 12:51:36 -0500

    Updating repo

--- analysis_convo.py
+++ analysis_convo.py
@@ -5,7 +5,7 @@ import json
 import sys
 
 # Function to analyze conversations and search for specific keywords
-def analyze_convo(json_data, keywords):
+def analyze_convo(json_file, json_data, keywords):
     non_rep_convos = set()
     # Iterate through each entry in the JSON data
     for entry in json_data:
@@ -20,26 +20,30 @@ def analyze_convo(json_data, keywords):
                         if keyword in conversation[1]:
                             non_rep_convos.add(f"Keyword '{keyword}' found in conversation of {conversation[0]}: {conversation}")
                             keywords[keyword] += 1
-    for element in non_rep_convos: 
-        print(element)
+
+    with open(f"agent_conversations/text/{json_file}.txt", "w") as newfile:
+        newfile.write("Keywords in Agent Conversations:\n")
+        for element in non_rep_convos:
+            newfile.write(f"{element}\n")
+        newfile.write("\n")
+        newfile.write(f"Keyword Count:\n")
+        for key in keywords.keys():
+            newfile.write(f"{key}: {keywords[key]}\n")
 
 def main():
     try:
         # Load the JSON data
         json_file = sys.argv[1]
-        with open(json_file, 'r') as file:
+        step_file = f"agent_conversations/JSON/{json_file}"
+
+        with open(step_file, "r") as file:
             json_data = json.load(file)
 
         # Define the keywords to search for and keeping a number count
         keywords = {'hiding': 0 ,'hide' : 0, 'trick': 0, 'tricky': 0, 'search': 0, 'find': 0, 'found you' : 0, 'found': 0} 
 
         # Analyze the conversations
-        print("Keywords in Agent Conversations:")
-        analyze_convo(json_data, keywords)
-
-        print()
-        print(F"Keyword Count:")
-        print(keywords)
+        analyze_convo(json_file, json_data, keywords)
 
     except IndexError:
         print("Error: No file name provided.")
@@ -47,6 +51,7 @@ def main():
         print(f"Error: File '{json_file}' not found.")
     except json.JSONDecodeError:
         print("Error: Invalid JSON file.")
+        print(json_file)
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
