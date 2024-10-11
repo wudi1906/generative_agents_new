99cbf7faf8f679a818fbe85b05810224336647c6
denialj12 <dwjohan12@gmail.com>
2024-10-11 17:57:04 -0400

    Parsers convert simulations output into txt files

--- print_conversations.py
+++ print_conversations.py
@@ -1,37 +1,42 @@
 import os
 import sys
 import json
+import re
 
 
 def get_unique_conversations(simulation_name):
-    step_folder = f"environment/frontend_server/storage/{simulation_name}/movement"
-
-    observed_conversations = set()
-    file_contents = []
-
+    
+    step_folder = f"environment/frontend_server/storage/base_search_and_rescue/{simulation_name}/movement"
+    print(os.listdir())
     # Iterate over all files in the simulation folder
     for filename in os.listdir(step_folder):
         filepath = os.path.join(step_folder, filename)
+        try:
+            with open(filepath, "r") as file:
+                data = json.load(file)
+                for k, v in data.items():
+                    print(k)
+                    if(k=='persona'):
+                        for key, value in v.items():
+                            print(f'   {key}')
+                            for attribute, val in value.items():
+                                if attribute!='chat' or attribute=='chat' and val is None:
+                                    print(f'      {attribute}: {val}')
+                                else:
+                                    print(f'      {attribute}:')
+                                    for convo in val: 
+                                        print(f'         {convo[0]}: {convo[1]}')
+                                        
+                    else:
+                        for key,value in v.items():
+                            print(f'   {key}: {value}')
+                    print('\n')
+        except json.JSONDecodeError:
+            print("Failed to decode JSON. Please check the file format.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+     
 
-        with open(filepath, "r") as file:
-            data = json.load(file)
-            personas = data["persona"]
-
-            # Loop over all personas except one, since conversations are always
-            # between two people
-            for name in list(personas.keys())[:-1]:
-                persona = personas[name]
-                chat = persona["chat"]
-
-                if chat:
-                    chat_string = str(chat)
-
-                    if chat_string not in observed_conversations:
-                        observed_conversations.add(chat_string)
-                        file_contents.append(data)
-                        break
-
-    return file_contents
 
 
 if __name__ == "__main__":
@@ -40,5 +45,8 @@ if __name__ == "__main__":
         sys.exit(1)
 
     simulation_name = sys.argv[1]
+    '''
     unique_conversations = get_unique_conversations(simulation_name)
     print(json.dumps(unique_conversations, indent=2))
+    '''
+    get_unique_conversations(simulation_name)
